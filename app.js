28f748f94729ddbd779659b9147a50e0b88bef9e
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2024-09-29 12:21:05 +0500

    handle single year, day, month

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var input = document.querySelector("input");
 var outputYear = document.getElementById("year-text");
 var outputMonth = document.getElementById("month-text");
 var outputDay = document.getElementById("day-text");
+var year = document.getElementById("year");
+var month = document.getElementById("month");
+var day = document.getElementById("day");
 
 btn.addEventListener("click", function () {
   var birthDate = new Date(input.value);
   var currentDate = new Date();
-  
+
   var years = currentDate.getFullYear() - birthDate.getFullYear();
   var months = currentDate.getMonth() - birthDate.getMonth();
   var days = currentDate.getDate() - birthDate.getDate();
@@ -20,6 +23,16 @@ btn.addEventListener("click", function () {
   if (months < 0) {
     years--;
     months += 12;
+  }
+
+  if (years == 1) {
+    year.textContent = "Year";
+  }
+  if (months == 1) {
+    month.textContent = "Month";
+  }
+  if (days == 1) {
+    day.textContent = "Day";
   }
 
   outputYear.textContent = years;
