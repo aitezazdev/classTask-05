39a578f9100ba744a8a9f06eb211660b271a0b01
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2024-09-29 11:33:38 +0500

    var declare and get elements by Id

---
+++ app.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector("button");
+var input = document.querySelector("input");
+var outputYear = document.getElementById("year-text");
+var outputMonth = document.getElementById("month-text");
+var outputDay = document.getElementById("day-text");
+
+btn.addEventListener("click", function() {
+    
+})
