3cba0e23a68af05eacfe69396f8fce1d2285a9f2
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2024-09-29 11:37:47 +0500

    btn functionality add

--- app.js
+++ app.js
@@ -5,5 +5,12 @@ var outputMonth = document.getElementById("month-text");
 var outputDay = document.getElementById("day-text");
 
 btn.addEventListener("click", function() {
-    
+    var date = new Date(input.value);
+    var currentDate = new Date();
+    var year = currentDate.getFullYear() - date.getFullYear();
+    var month = currentDate.getMonth() - date.getMonth();
+    var day = currentDate.getDate() - date.getDate() + 1;
+    outputYear.innerHTML = year;
+    outputMonth.innerHTML = month;
+    outputDay.innerHTML = day;
 })
